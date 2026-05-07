ff03201100ad9d321c2401d221266e19436bb697
Hz2i <stefan.amarfii52@gmail.com>
2026-05-07 15:15:01 +0200

    Fixed some inputs | Added options to provide battery and solar cell types

--- objects_prelim/power_sizing.py
+++ objects_prelim/power_sizing.py
@@ -3,41 +3,34 @@ from materials_components import solar_panel, battery, fuel_cell
 
 
 class power_storage:
-    def __init__(self, power_req, latitude, days_from_solstice, DOD):                     # Initialise known/computed values and constants
+    def __init__(self, power_req, latitude, days_from_solstice, DOD, battery_input=battery()):                     # Initialise known/computed values and constants
         daylight_analysis = solar_incidence(latitude, days_from_solstice)
         daylight_analysis.daylight_cycle()
         self.power_req = power_req
         self.daylight_time = daylight_analysis.daylight_time
         self.DOD = DOD # depth of discharge
-        bat = battery()
-        self.massEnergy = bat.massEnergy                     # Mass energy density [J/kg]
-        self.volumeEnergy = bat.volumeEnergy             # Volumetric energy density [J/m^3]
-        self.massRho = bat.massRho    # Mass density [kg/m^3]
+        self.bat = battery_input
 
     def compute_weight_volume(self):        # Compute weight and volume of power storage components
         energy_req = self.power_req*(86400-self.daylight_time)/self.DOD
-        self.mass = energy_req/self.massEnergy                  # Mass of Energy Storage System
-        self.volume = energy_req/self.volumeEnergy              # Volume of Energy Storage System
+        self.mass = energy_req/self.bat.massEnergy                  # Mass of Energy Storage System
+        self.volume = energy_req/self.bat.volumeEnergy              # Volume of Energy Storage System
 
 class power_generation:
-    def __init__(self, power_req, latitude, days_from_solstice):                     # Initialise known/computed values and constants
+    def __init__(self, power_req, latitude, days_from_solstice, solar_panel_input=solar_panel()):                     # Initialise known/computed values and constants
         daylight_analysis = solar_incidence(latitude,days_from_solstice)
         daylight_analysis.daylight_cycle()
         self.power_req = power_req
         self.daylight_time = daylight_analysis.daylight_time
         self.avg_incidence = daylight_analysis.avg_incidence
-        solar = solar_panel()
-        self.efficiency = solar.efficiency      # Efficiency
-        self.surfRho = solar.surfRho            # Surface mass density [kg/m^2]
-        self.powLimS = solar.powLimS            # Power limit per m^2 [W/m^2]
-        self.powLimM = solar.powLimM            # Power limit per kg [W/kg]
+        self.solar = solar_panel_input
 
     def compute_weight_surface(self):       # Compute weight and surface area required for power generation components
         avg_incidence = np.pi/2 - self.avg_incidence # converting to incidence angle wrt normal
-        power_per_m = np.minimum(self.powLimS,self.efficiency*1378.0*np.cos(avg_incidence))
+        power_per_m = np.minimum(self.solar.powLimS,self.solar.efficiency*1378.0*np.cos(avg_incidence))
         self.daylight_power_req = self.power_req * 86400 / self.daylight_time # watts
         self.area = self.daylight_power_req/power_per_m # m^2
-        self.mass = self.area*self.surfRho # kg
+        self.mass = self.area*self.solar.surfRho # kg
 
 class solar_incidence:
     def __init__(self, latitude, days_from_solstice = 0):                     # Initialise known/computed values and constants
@@ -67,6 +60,9 @@ payload_peak = 150 # W
 payload_frac = 0.1 # [-]
 margin = 300 # W
 
+bat = battery()
+solar_c = solar_panel()
+
 power_req = power_required(mass,LD,prop_eff,V_cruise,payload,payload_peak,payload_frac,margin)
 latitude = 50 # deg
 days_from_solstice = 0 # days
@@ -75,10 +71,10 @@ DoD = 0.8 # depth of discharge
 solar_properties = solar_incidence(latitude,days_from_solstice)
 solar_properties.daylight_cycle()
 
-energy_storage = power_storage(power_req.output,latitude,days_from_solstice,DoD)
+energy_storage = power_storage(power_req.output,latitude,days_from_solstice,DoD,bat)
 energy_storage.compute_weight_volume()
 
-energy_generation = power_generation(power_req.output,latitude,days_from_solstice)
+energy_generation = power_generation(power_req.output,latitude,days_from_solstice,solar_c)
 energy_generation.compute_weight_surface()
 
 print(f'\nAt a latitude of {latitude} degrees: \n' + 
