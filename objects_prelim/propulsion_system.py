ca9ef74bf04132c13bd05cca705c9921d01d8b52
Hz2i <stefan.amarfii52@gmail.com>
2026-05-08 14:15:10 +0200

    Started reformatting classes according to work plan

---
+++ objects_prelim/propulsion_system.py
@@ -0,0 +1,9 @@
+import numpy as np
+from objects.class1_est import struct_groups            # Import preliminary weights
+from objects.flight_performance import mission_profile  # Import matching diagram methods and values (mainly acquiring T/W)
+
+
+class motor:
+    def __init__(self):                                 # Initialise with known constants/statistics from literature
+
+    def compute_size_power(self):                       # Compute size/weight/power values for the propulsion system
