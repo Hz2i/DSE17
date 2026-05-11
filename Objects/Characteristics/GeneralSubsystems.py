ecf5adcdfe1cb6ca9e360300b1701f6ee3f7cf7c
MartijnVDV1 <m.l.a.vandervoort@student.tudelft.nl>
2026-05-11 16:24:42 +0200

    comms updates

--- Objects/Characteristics/GeneralSubsystems.py
+++ Objects/Characteristics/GeneralSubsystems.py
@@ -15,12 +15,14 @@ class ComputerSystem:
         return self.comp_electrical_power_required, self.comp_mass, self.comp_volume, self.comp_x_pos                                                   
 
 class CommunicationSystem:
-    def __init__(self):                                     # Initialise with proper values
-        self.mass = self.compute_communication_system()
-        self.volume = 0.0
-        self.x_pos = 0.0
-        self.power_required = 0.0
+    def __init__(self, required_bitrate=2 * 10**6, required_distance=400 * 10**3, link_budget=10**(-12), transmit_power):                                     # Initialise with proper values
+        self.bitrate_required = required_bitrate        # bps, payload needs 1 MBit/s, so 2 Mbit/s is a conservative estimate
+        self.distance = required_distance                    # m, 400 km horizontally, approx. 20 km vertically
+        self.link_budget = link_budget                 # 
+        self.transmit_power = transmit_power        # W
+        self.comms_mass_density = 0.030             # kg/W, typical for HAPS
     def compute_communication_system(self):                          # Compute all relevant characteristics of the communication system
+        self.comms_mass = self.adsb_mass + self.elt_mass + self.ssr_mass + self.comms_mass_density
         return None
 
 class FlightConditionsSystem:
@@ -43,9 +45,7 @@ class PayloadSystem:
 
 class ControlSystem:
     def __init__(self):                                     # Initialise with proper values
-        self.mass = self.compute_control_system()
-        self.volume = 0.0
-        self.x_pos = 0.0
         self.power_required = 0.0
+
     def compute_control_system(self):                          # Compute all relevant characteristics of the control system
         return None
