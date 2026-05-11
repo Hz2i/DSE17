b6c02b63bb410fcdd8df2b4a4e0f234252fb021a
MartijnVDV1 <m.l.a.vandervoort@student.tudelft.nl>
2026-05-11 12:15:01 +0200

    comp system estimated

--- Objects/Characteristics/GeneralSubsystems.py
+++ Objects/Characteristics/GeneralSubsystems.py
@@ -1,28 +1,51 @@
 import numpy as np
 
 class ComputerSystem:
-    def __init__(self):                                     # Initialise with proper values
-        pass
+    def __init__(self, required_flops):                                     # Initialise with proper values
+        self.computing_power_required = required_flops # FLOPS, order of 10^9 - 10^10 probably
+        self.computing_efficiency = 2 * 10**9  # FLOPS/W, Raspberry Pi 5
+        self.mass_density = 2.5 * 10**(-12)  # kg/FLOPS, Raspberry Pi 5
+        self.volume_density = 95.2 * 10**(-6)  # m^3/FLOPS, Raspberry Pi 5
+     
     def compute_computer_system(self):                          # Compute all relevant characteristics of the computer system
-        return None
+        self.comp_electrical_power_required = self.computing_power_required / self.computing_efficiency # W
+        self.comp_mass = self.computing_power_required * self.mass_density               
+        self.comp_volume = self.computing_power_required * self.volume_density     
+        self.comp_x_pos = 0.0  # Needs implementation
+        return self.comp_electrical_power_required, self.comp_mass, self.comp_volume, self.comp_x_pos                                                   
+
 class CommunicationSystem:
     def __init__(self):                                     # Initialise with proper values
-            pass
+        self.mass = self.compute_communication_system()
+        self.volume = 0.0
+        self.x_pos = 0.0
+        self.power_required = 0.0
     def compute_communication_system(self):                          # Compute all relevant characteristics of the communication system
         return None
+
 class FlightConditionsSystem:
     def __init__(self):                                     # Initialise with proper values
-        self.mass = None
-        self.x_pos = None
+        self.mass = 0.0                                     # Currently initialised with 0; Class 2 estimation methods required!
+        self.volume = 0.0
+        self.x_pos = 0.0
+        self.power_required = 0.0
     def compute_flight_conditions_system(self):                          # Compute all relevant characteristics of the flight conditions system
         return None
+
 class PayloadSystem:
     def __init__(self):                                     # Initialise with proper values
-        pass
+        self.mass = self.compute_payload_system()
+        self.volume = 0.0
+        self.x_pos = 0.0
+        self.power_required = 0.0
     def compute_payload_system(self):                          # Compute all relevant characteristics of the payload system
         return None
+
 class ControlSystem:
     def __init__(self):                                     # Initialise with proper values
-        pass
+        self.mass = self.compute_control_system()
+        self.volume = 0.0
+        self.x_pos = 0.0
+        self.power_required = 0.0
     def compute_control_system(self):                          # Compute all relevant characteristics of the control system
         return None
