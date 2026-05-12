a8f8486aaf19e253fb111ec50bcbf671619d0c19
SJYPvanStrien <sjypvanstrien@tudelft.nl>
2026-05-12 11:37:47 +0200

    push this eff

--- Objects/Performance/ClimbEnveloppe.py
+++ Objects/Performance/ClimbEnveloppe.py
@@ -2,7 +2,8 @@ import numpy as np
 
 class ClimbEnveloppe:
     def __init__(self):                                 # Initialise with required values (add inputs after self, as per necessity)
-        pass
+        self.lat_range = [0,5,10,15,20,25,30]  # Latitude range in degrees
+        self.alt = 60000*0.3048  # Altitude in meters (converted from feet)
     def compute_climb_enveloppe(self):
         return None
     
