8d3e931788570aad0b3a5b2fb8bbaf2ac5e2e7ee
MartijnVDV1 <m.l.a.vandervoort@student.tudelft.nl>
2026-05-12 17:15:25 +0200

    Setup framework for contingency management

--- Objects/ContingencyManagement/contingency_management.py
+++
@@ -1,8 +0,0 @@
-"""
-This file is intended to handle the contingencies applied during the design process.
-It will take the current values of the design parameters and check them against the target values.
-If the current value exceeds the target value, it will raise a warning.
-The contingencies have to be updated manually by the risk manager or the risk manager has to be informed of intended changes.
-Ideally, the contingencies reduce to zero when the design is finalised.
-"""
-
