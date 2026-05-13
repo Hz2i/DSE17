c9d8791234ea2b8559eb1993c1732ff5e87b5431
Jasper van Ruiten <j.c.vanruiten@student.tudelft.nl>
2026-05-13 14:52:06 +0200

    Added pytest for control system

--- tests/Characteristics/test_GeneralSubsystems.py
+++ tests/Characteristics/test_GeneralSubsystems.py
@@ -1,6 +1,7 @@
 import pytest
 from Objects.Characteristics.GeneralSubsystems import FlightConditionsSystem
 from Objects.Characteristics.GeneralSubsystems import PayloadSystem
+from Objects.Characteristics.GeneralSubsystems import ControlSystem
 
 def test_flight_conditions_system():
     fcs = FlightConditionsSystem()
@@ -25,3 +26,14 @@ def test_payload_system():
     assert payload.PS_mass == pytest.approx(expected_mass)
     assert payload.PS_volume == pytest.approx(expected_volume)
     assert payload.PS_power == pytest.approx(expected_power)
+
+def test_control_system():
+    control = ControlSystem()
+
+    expected_mass = (control.actuator_mass + control.mass_cables + control.mass_pushrod + control.joints_mass_ratio * control.mass_pushrod) * 4
+    expected_volume = (control.actuator_volume + control.volume_cable + control.volume_pushrod) * 4
+    expected_power = (control.actuator_power + control.power_loss_cable) * 4
+
+    assert control.CS_mass == pytest.approx(expected_mass)
+    assert control.CS_volume == pytest.approx(expected_volume)
+    assert control.CS_power_required == pytest.approx(expected_power)
