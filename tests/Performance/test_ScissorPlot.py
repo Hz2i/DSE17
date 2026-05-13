cfc8350d86465d60d8600196c3adab0bfb20922c
Jasper van Ruiten <j.c.vanruiten@student.tudelft.nl>
2026-05-13 17:13:28 +0200

    Started working on tests

---
+++ tests/Performance/test_ScissorPlot.py
@@ -0,0 +1,18 @@
+import pytest
+import numpy as np
+from Objects.Performance.ScissorPlot import ScissorPlot
+
+# ==============================
+# CODE VERIFICATION TESTS
+# ==============================
+
+def __init__(self):
+    self.MAC = 2.0
+    self.Cm_ac = -0.05
+    self.CL_max = 1.2
+    self.S = 20.0
+    self.AR = 10.0
+    self.b = 14.0
+    self.x_ac = 0.25
+    
+    
