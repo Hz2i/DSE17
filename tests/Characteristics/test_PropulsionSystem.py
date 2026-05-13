dfcee6f4dad41a35de829b19a495fc65ab471d3f
luuk <97060135+luukmvb@users.noreply.github.com>
2026-05-13 16:08:59 +0200

    prop test

--- tests/Characteristics/test_PropulsionSystem.py
+++ tests/Characteristics/test_PropulsionSystem.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import ambiance as am
 from Objects.Characteristics.PropulsionSystem import PropulsionSystem
 
 # ==============================
@@ -29,9 +30,10 @@ def test_propulsion_system_null_values():
     assert propulsion.power_required == 0.0
     # assert propulsion.lambda_adv == pytest.approx(0.19098593171027442)
 
-    # ORDER OF MAGNITUDE TESTS
+
+# ORDER OF MAGNITUDE TESTS
 def test_propulsion_system_order_of_magnitude():
-    """Test order-of-magnitude for key outputs."""
+    """Test order-of-magnitude for key outputs"""
     propulsion = PropulsionSystem(
         plotdata=False,
         T=100,
@@ -50,3 +52,23 @@ def test_propulsion_system_order_of_magnitude():
     assert 100.0 < propulsion.power_required < 4000.0
     # assert 0.1 < propulsion.lambda_adv < 1.0
     
+# ASSUMED CONSTANTS CROSS-VERIFICATION TESTS
+def test_propulsion_system_constants():
+    """Test that constants used in calculations are as expected"""
+    propulsion = PropulsionSystem(
+        plotdata=False,
+        T=100,
+        velocity=25,
+        alt=5000,
+        rpm=1000,
+        torque=4,
+        motor_temp=20,
+        propeller_diameter=2.5,
+    )
+
+    assert am.Atmosphere(propulsion.alt).density[0] == pytest.approx(0.736, abs=0.01)  # Air density at 5000 m
+    assert am.Atmosphere(propulsion.alt).speed_of_sound[0] == pytest.approx(320.5, abs=0.1)  # Speed of sound at 5000 m
+
+
+# UNIT TEST 
+# TODO, unit tests when finalized
