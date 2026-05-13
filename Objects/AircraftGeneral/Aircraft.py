383b98f9f10546788ce44f575f91c0219f9bc71f
Hz2i <stefan.amarfii52@gmail.com>
2026-05-13 16:18:03 +0200

    Adjusted surface area sizing code

--- Objects/AircraftGeneral/Aircraft.py
+++ Objects/AircraftGeneral/Aircraft.py
@@ -24,6 +24,7 @@ class Aircraft:
         self.ctrls = ctrls
         self.pow_store = None
         self.solar = None
+        self.prop = None
 
         self.TAS = TAS
         self.h = h
@@ -33,6 +34,8 @@ class Aircraft:
         self.gamma = gamma
 
         self.CL_CD = None
+        self.CD0 = None
+        self.CL_opt = None
 
         self.size_wing()
 
@@ -47,30 +50,38 @@ class Aircraft:
     def size_wing(self):
         err = 1.0e3
         iterations = 0
+        damping = 0.25
+        surface_check = True
 
-        while err > 0.025 and iterations < 50:
+        # Ensure the first iteration of area sizing undershoots:
+        self.wing.S = self.wing.S / 5.0
+
+        # Ensure current Cl and CD values are in scope:
+        CL_current = None
+        CD_current = None
+
+        while surface_check:
             self.wing.compute_oswald_eff()
             self.wing.zero_lift_drag(rho_cruise=am.Atmosphere(self.h).density[0], V_cruise=self.TAS, M=0.1)
             self.fus.zero_lift_drag(rho_cruise=am.Atmosphere(self.h).density[0], V_cruise=self.TAS)
             self.emp.zero_lift_drag(rho_cruise=am.Atmosphere(self.h).density[0], V_cruise=self.TAS, M=0.1)
 
+            self.CD0 = (self.fus.CD0 + self.wing.CD0 + self.emp.CD0)/self.wing.S
+            self.CL_opt = (3.0 * self.CD0 * np.pi * self.wing.AR * self.wing.e)**0.5
 
-            CD0 = (self.fus.CD0 + self.wing.CD0 + self.emp.CD0)/self.wing.S
-            CL = (3.0 * CD0 * np.pi * self.wing.AR * self.wing.e)**0.5
+            TAS_opt = (self.MTOW*self.const.g / (0.5 * am.Atmosphere(self.h).density[0] * self.wing.S * self.CL_opt))**0.5
 
-            print("Oswald efficiency:", self.wing.e)
-            print("CD0:", CD0)
-            print("CL:", CL)
-
-            CD = CD0 + CL**2/(np.pi*self.wing.AR*self.wing.e)
-            print("CD:", CD)
-            CL_CD = CL/CD
-            self.CL_CD = CL_CD
-            print("CL/CD:", CL_CD)
+            if TAS_opt > self.TAS:
+                CL_current = self.CL_opt
+                CD_current = self.CD0 + CL_current**2/(np.pi*self.wing.AR*self.wing.e)
+                self.CL_CD = CL_current/CD_current
+            else:
+                CL_current = self.MTOW*self.const.g / (0.5 * am.Atmosphere(self.h).density[0] * self.TAS**2 * self.wing.S)
+                CD_current = self.CD0 + CL_current**2/(np.pi*self.wing.AR*self.wing.e)
+                self.CL_CD = CL_current/CD_current
 
             self.T_req = self.MTOW*self.const.g/self.CL_CD + self.MTOW*self.const.g * np.sin(np.radians(self.gamma))
             self.prop = PropulsionSystem(T=self.T_req, velocity=self.TAS, alt=self.h, rpm=1000.0, torque=4.0, motor_temp=-40.0)
-            print("Propulsive efficiency:", self.prop.overall_eff)
 
             self.Pow_motor = self.prop.power_required
             self.Pow_req = self.compute_subsys_pow() + self.Pow_motor
@@ -80,10 +91,19 @@ class Aircraft:
             self.solar = power_generation(self.Pow_req, latitude=self.lat, days_from_solstice=self.day_margin)
             self.solar.compute_weight_surface()
 
-            err = ((self.solar.area - self.wing.S)**2)**0.5 / self.solar.area
-            self.wing.S = self.solar.area
-            iterations += 1
+            if self.solar.area < self.wing.S:
+                surface_check = False
+            else:
+                self.wing.S += np.maximum(0.1, damping * (self.solar.area - self.wing.S))
+                iterations += 1
+                # print("Inner iteration:", iterations)
+                # print("Surface difference:", self.solar.area - self.wing.S)
 
+        print("Optimal CL:", self.CL_opt)
+        print("CL:", CL_current)
+        print("CL/CD:", self.CL_CD)
+        print("Oswald efficiency:", self.wing.e)
+        print("Propulsive efficiency:", self.prop.overall_eff)
 
 
     def compute_subsys_pow(self):
