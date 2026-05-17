Fixed CD0 computation Error

Objects/AircraftGeneral/Aircraft.py
@@ -2,7 +2,7 @@ import numpy as np
 import ambiance as am
 
 
-from Objects.Characteristics.Airframe import wing, fuselage, empennage
+from Objects.Characteristics.Airframe import wing, fuselage, empennage, nacelles
 from Objects.Characteristics.GeneralSubsystems import ComputerSystem, CommunicationSystem, FlightConditionsSystem, PayloadSystem, ControlSystem
 from Objects.Characteristics.PowerSystem_sizing import power_storage, power_generation
 from Objects.Characteristics.PropulsionSystem import PropulsionSystem
@@ -10,13 +10,14 @@ from Objects.Constants import Constants
 
 
 class Aircraft:
-    def __init__(self, MTOW_guess=100.0, TAS=20.0, h=18000.0, gamma=0.0, lat=50.0, day_margin=0, DoD=0.8, wing=wing(), fus=fuselage(), emp=empennage(), comp=ComputerSystem(), comms=CommunicationSystem(), flight_con=FlightConditionsSystem(), payload=PayloadSystem(), ctrls=ControlSystem()):
+    def __init__(self, MTOW_guess=100.0, TAS=20.0, h=18000.0, gamma=0.0, lat=50.0, day_margin=0, DoD=0.8, wing=wing(), fus=fuselage(), emp=empennage(), nac=nacelles(), comp=ComputerSystem(), comms=CommunicationSystem(), flight_con=FlightConditionsSystem(), payload=PayloadSystem(), ctrls=ControlSystem()):
         self.MTOW = MTOW_guess
         self.const = Constants()
 
         self.wing = wing
         self.fus = fus
         self.emp = emp
+        self.nac = nac
         self.comp = comp
         self.comms = comms
         self.flight_con = flight_con
@@ -68,11 +69,12 @@ class Aircraft:
             self.wing.zero_lift_drag(rho_cruise=am.Atmosphere(self.h).density[0], V_cruise=self.TAS, M=0.1)
             self.fus.zero_lift_drag(rho_cruise=am.Atmosphere(self.h).density[0], V_cruise=self.TAS)
             self.emp.zero_lift_drag(rho_cruise=am.Atmosphere(self.h).density[0], V_cruise=self.TAS, M=0.1)
+            self.nac.zero_lift_drag(rho_cruise=am.Atmosphere(self.h).density[0], V_cruise=self.TAS)
 
-            self.CD0 = (self.fus.CD0 + self.wing.CD0 + self.emp.CD0)/self.wing.S
+            self.CD0 = (self.fus.CD0 + self.wing.CD0 + self.emp.CD0 + self.nac.CD0)/self.wing.S
             self.e = 1/(0.38*self.CD0*np.pi*self.wing.AR + 1/(self.wing.e*(1-2*(self.fus.D/self.wing.b)**2)))
 
-            self.CL_opt = (self.CD0 * np.pi * self.wing.AR * self.e)**0.5
+            self.CL_opt = (3* self.CD0 * np.pi * self.wing.AR * self.e)**0.5
 
             TAS_opt = (self.MTOW*self.const.g / (0.5 * am.Atmosphere(self.h).density[0] * self.wing.S * self.CL_opt))**0.5
 

Objects/Characteristics/Airframe.py
@@ -55,7 +55,7 @@ class wing:
         Reynolds = np.minimum(rho_cruise*V_cruise*chord/mu,38.21*(chord/k)**1.053)
 
         Cf_l = 1.328/np.sqrt(Reynolds)
-        Cf_t = 0.455/(np.log(Reynolds)**2.58)
+        Cf_t = 0.455/(np.log10(Reynolds)**2.58)
 
         f_l = 0.1
 
@@ -103,7 +103,7 @@ class fuselage:
         Reynolds = np.minimum(rho_cruise*V_cruise*length/mu,38.21*(length/k)**1.053)
 
         Cf_l = 1.328/np.sqrt(Reynolds)
-        Cf_t = 0.455/(np.log(Reynolds)**2.58)
+        Cf_t = 0.455/(np.log10(Reynolds)**2.58)
 
         f_l = 0.0
 
@@ -164,10 +164,10 @@ class empennage:
         Reynolds_v = np.minimum(rho_cruise*V_cruise*chord_v/mu,38.21*(chord_v/k)**1.053)
 
         Cf_l_h = 1.328/np.sqrt(Reynolds_h)
-        Cf_t_h = 0.455/(np.log(Reynolds_h)**2.58)
+        Cf_t_h = 0.455/(np.log10(Reynolds_h)**2.58)
 
         Cf_l_v = 1.328/np.sqrt(Reynolds_v)
-        Cf_t_v = 0.455/(np.log(Reynolds_v)**2.58)
+        Cf_t_v = 0.455/(np.log10(Reynolds_v)**2.58)
 
         f_l = 0.1 # laminar fraction
 
@@ -178,3 +178,35 @@ class empennage:
         FF_v = (1+0.6/self.foil_v.thickness_pos * self.foil_v.max_thickness + 100 * self.foil_v.max_thickness**4)*(1.34*M**0.18*(np.cos(self.qc_sweep_v))**0.28)
 
         self.CD0 = (Cf_h * FF_h * S_wet_h + Cf_v * FF_v * S_wet_v) ## 1/S needs to be applied externally
+
+
+class nacelles:
+    def __init__(self,nr_of_engines = 4,diameter = 0.5, length = 1):
+        self.nr_of_engines = nr_of_engines
+        self.D = diameter
+        self.L = length
+
+        
+    def zero_lift_drag(self, rho_cruise, V_cruise):
+        self.Sw = np.pi*self.D/4 * (1/(3*self.L**2)*((4*self.L**2+self.D**2/4)**1.5))
+
+
+        mu = 1.4216e-5 # at 60,000 ft (18,500 m)
+        k = 0.405e-5 # surface roughness of metal sheets
+
+        Reynolds = np.minimum(rho_cruise*V_cruise*self.L/mu,38.21*(self.L/k)**1.053)
+
+        Cf_l = 1.328/np.sqrt(Reynolds)
+        Cf_t = 0.455/(np.log10(Reynolds)**2.58)
+
+        f_l = 0.0
+
+        Cf = f_l*Cf_l + (1-f_l)*Cf_t
+
+        if self.D == 0:
+            f = 1
+        else:
+            f = (self.L)/self.D
+
+        self.CD0 = self.nr_of_engines*self.Sw * (Cf*(1+0.35/f)) ## 1/S needs to be applied externally
+

prelim_sizing.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from Objects.Characteristics.Airframe import wing, fuselage, empennage
+from Objects.Characteristics.Airframe import wing, fuselage, empennage, nacelles
 from Objects.Characteristics.GeneralSubsystems import ComputerSystem, CommunicationSystem, FlightConditionsSystem, PayloadSystem, ControlSystem
 from Objects.Characteristics.PropulsionSystem import PropulsionSystem
 from Objects.Characteristics.ReferenceGeometries import *
@@ -9,19 +9,20 @@ from Objects.Constants import Constants
 from Objects.AircraftGeneral.Aircraft import Aircraft
 
 
-powM_frac_target = 0.45     # From the NASA paper (mass fraction of the power system)
+powM_frac_target = 0.7     # From the NASA paper (mass fraction of the power system)
 MTOW_initial = 120.0
 TAS_initial = 25.0
 gamma = 0.0
-h_cruise = 18000.0
-lat = 40.0
+h_cruise = 18500.0
+lat = 30.0
 day_margin = 0
 DoD = 0.8
 night_time = 0.0
 
-wing_geo = wing(A=26.5, qc_sweep=0.0, taper=1.0, dihedral=5.0*np.pi/180.0)
+wing_geo = wing(A=26.5, qc_sweep=0.0*np.pi/180, taper=1.0, dihedral=5.0*np.pi/180.0)
 fus_geo = fuselage()
 emp_geo = empennage()
+nac_geo = nacelles(nr_of_engines=0)
 
 # General subsystem parameters may be changed using the following (commented) code block; Sensible defaults should already be implemented
 
@@ -35,14 +36,14 @@ emp_geo = empennage()
 MTOW = MTOW_initial
 
 # Compute intial error:
-AHAPS = Aircraft(MTOW_guess=MTOW, TAS=TAS_initial, gamma=gamma, lat=lat, day_margin=day_margin, DoD=DoD, wing=wing_geo, fus=fus_geo, emp=emp_geo)
+AHAPS = Aircraft(MTOW_guess=MTOW, TAS=TAS_initial, gamma=gamma, lat=lat, day_margin=day_margin, DoD=DoD, wing=wing_geo, fus=fus_geo, emp=emp_geo, nac=nac_geo)
 powM_frac = (AHAPS.pow_store.mass + AHAPS.solar.mass)/MTOW
 error = abs(powM_frac - powM_frac_target)/powM_frac_target
 
 
 iterations = 0
 while error > 1e-3:
-    AHAPS = Aircraft(MTOW_guess=MTOW, TAS=TAS_initial, gamma=gamma, lat=lat, day_margin=day_margin, DoD=DoD, wing=wing_geo, fus=fus_geo, emp=emp_geo)
+    AHAPS = Aircraft(MTOW_guess=MTOW, TAS=TAS_initial, gamma=gamma, lat=lat, day_margin=day_margin, DoD=DoD, wing=wing_geo, fus=fus_geo, emp=emp_geo, nac=nac_geo)
     powM_frac = (AHAPS.pow_store.mass + AHAPS.solar.mass)/MTOW
     error = abs(powM_frac - powM_frac_target)/powM_frac_target
 