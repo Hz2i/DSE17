bfd8dbf9d9de087c95983583dfeef5c261eb748b
Federico <johndoe@example.com>
2026-05-18 14:19:02 +0200

    started test_C02.py but need materials & weights lists

---
+++ tests/Performance/test_C02.py
@@ -0,0 +1,9 @@
+import pytest
+from Objects.Performance.CO2 import calculate_co2_emissions_MaterialWeight
+
+# ==============================
+# CODE VERIFICATION TESTS
+# ==============================
+
+def test_calculate_co2_emissions_MaterialWeight():
+    calculate_co2_emissions_MaterialWeight() >
