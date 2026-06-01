18ba2e169c6dc1f47428aa601e2589e7bce0c6f2
Hz2i <stefan.amarfii52@gmail.com>
2026-06-01 10:16:01 +0200

    Initialised templates for structures and control code

---
+++ objects_detailed/Methods/Controls.py
@@ -0,0 +1,14 @@
+import numpy as np
+import aerosandbox as asb
+
+
+def compute_ctrl_der():             # Compute control derivatives
+    pass
+
+
+def compute_stab_pars():            # Compute stability parameters
+    pass
+
+
+def check_reversal():               # Check for aileron reversal
+    pass
---
+++ objects_detailed/Methods/StructuralAnalysis.py
@@ -0,0 +1,14 @@
+import numpy as np
+import aerosandbox as asb
+
+
+def internal_loading():              # Implement method for computing internal loading (lift distribution potentially taken from VLM analysis)
+    pass
+
+def stress_analysis():              # Implement method to compute maximum stresses
+    pass
+
+def buckling_analysis():            # Analyze buckling performance
+
+def deflection_analysis():          # Compute all relevant deflections
+    pass
