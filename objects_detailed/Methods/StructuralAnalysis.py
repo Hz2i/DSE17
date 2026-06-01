9e81a490c73c98ca46a43b580b09080250686664
Hz2i <stefan.amarfii52@gmail.com>
2026-06-01 10:21:01 +0200

    Incremental additions

--- objects_detailed/Methods/StructuralAnalysis.py
+++ objects_detailed/Methods/StructuralAnalysis.py
@@ -2,13 +2,17 @@ import numpy as np
 import aerosandbox as asb
 
 
-def internal_loading():              # Implement method for computing internal loading (lift distribution potentially taken from VLM analysis)
+def internal_loading():             # Implement method for computing internal loading (lift distribution potentially taken from VLM analysis)
     pass
 
 def stress_analysis():              # Implement method to compute maximum stresses
     pass
 
 def buckling_analysis():            # Analyze buckling performance
+    pass
 
 def deflection_analysis():          # Compute all relevant deflections
+    pass
+
+def dynamic_analysis():             # Check dynamic loads
     pass
