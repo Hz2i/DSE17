c589943e546580ad6986011833773dc85c72180d
Ian <ianghys@yahoo.com>
2026-06-04 15:41:11 +0200

    Updated Solar Panel and Battery Parameters based on new data + inefficiencies

--- objects_detailed/Characteristics/Components_Materials.py
+++ objects_detailed/Characteristics/Components_Materials.py
@@ -7,7 +7,7 @@ class material:
 
 
 class solar_panel:
-    def __init__(self, eff=0.20, rhoS=0.665, powS=200.0, powM=1000.0):                 # Provide method to define solar panel types with certain parameters
+    def __init__(self, eff=0.22*0.97**2*0.95, rhoS=0.400, powS=270.0, powM=500.0):                 # Provide method to define solar panel types with certain parameters
         self.efficiency = eff           # Efficiency
         self.surfRho = rhoS             # Surface mass density [kg/m^2]
         self.powLimS = powS             # Power limit per m^2 [W/m^2]
@@ -15,12 +15,12 @@ class solar_panel:
 
 
 class battery:
-    def __init__(self, E_m=400.0, E_vol=872.0):                 # Provide method to define battery types with certain parameters
+    def __init__(self, E_m=400.0*0.96, E_vol=872.0*0.96):                 # Provide method to define battery types with certain parameters
         self.massEnergy = E_m*3600.0                        # Mass energy density [J/kg]
         self.volumeEnergy = E_vol*3600.0/0.001              # Volumetric energy density [J/m^3]
         self.massRho = self.volumeEnergy/self.massEnergy    # Mass density [kg/m^3]
-        self.cycle_limit_nr=400                             # Cycle limit
-        self.cycle_limit_degradation=0.2                    # Cycle degradation at limit (fraction)
+        self.cycle_limit_nr=700                             # Cycle limit
+        self.cycle_limit_degradation=0.1                    # Cycle degradation at limit (fraction)
 
 
 class fuel_cell:
