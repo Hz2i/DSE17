52c3002a43470f6b14fa41c0f6ca87c42a53f47a
Ian <ianghys@yahoo.com>
2026-06-17 11:27:56 +0200

    added outputs

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ struct_frac_prev = 0.35
 gen_subsys_frac_prev = 0.05
 
 
-MTOW_initial = 300.0
+MTOW_initial = 318.0
 TAS_initial = 25.0
 gamma = 0.0
 h_cruise = 18500.0
 lat = 30.0
 day_margin = 0
+use_batt = True
 use_batt = True
 energy_delta = 0.0
 DoD = 0.8
